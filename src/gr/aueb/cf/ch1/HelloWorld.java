f664cc8951b22d04e6417842ff3c84d4b4e09de3
Dimos Tzanou <d.tzanou91@gmail.com>
2024-12-10 23:37:00 +0200

    initial comit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.ch1;
+
+/**
+ *
+ */
+public class HelloWorld {
+
+    public static void main( String[]args) {
+        /*
+         *
+         *
+         */
+
+
+        System.out.println("HELL WORLD");
+        System.out.print("HELL WORLD"); //awda
+        System.out.println();
+
+
+    }
+
+}
