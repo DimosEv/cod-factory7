693058ed6adf284f97a2317479e538f8271a5428
Dimos Tzanou <d.tzanou91@gmail.com>
2025-01-03 23:48:52 +0200

    Student data class with setters and getters

---
+++ src/gr/aueb/cf/ch12/Student.java
@@ -0,0 +1,32 @@
+package gr.aueb.cf.ch12;
+
+public class Student {
+    int id;
+    String firstname;
+    String lastname;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getFirstname(){
+        return firstname;
+    }
+
+    public void setFirstname(String firstname){
+        this.firstname = firstname;
+    }
+
+    public String getLastname(){
+        return lastname;
+    }
+
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
+        }
+
+
