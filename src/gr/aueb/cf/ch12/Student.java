e9f89f5d42add5da533672b5b3b6d259d9dc2414
Dimos Tzanou <d.tzanou91@gmail.com>
2025-01-04 23:56:15 +0200

    uppdated  with  overloaded constructor

--- src/gr/aueb/cf/ch12/Student.java
+++ src/gr/aueb/cf/ch12/Student.java
@@ -1,32 +1,54 @@
 package gr.aueb.cf.ch12;
 
+/**
+ *  Data class.
+ *  POJO (Plain Old Java Object)
+ *  JAva Bean.
+ *
+ */
+
 public class Student {
     int id;
     String firstname;
     String lastname;
 
-    public int getId(){
+    // Default Constructor
+    public Student() {
+    }
+
+    // Overloaded Constructor
+    public Student(int id, String fristname, String lastname){
+        this.id = id;
+        this.firstname = fristname;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
         return id;
     }
 
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
-    public String getFirstname(){
+
+    public String getFirstname() {
         return firstname;
     }
 
-    public void setFirstname(String firstname){
+    public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
 
-    public String getLastname(){
+    public String getLastname() {
         return lastname;
     }
 
-    public void setLastname(String lastname){
+    public void setLastname(String lastname) {
         this.lastname = lastname;
     }
-        }
+
+
+}
+
 
 
